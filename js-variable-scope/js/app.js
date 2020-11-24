1cdc77d72ee59cbd5b68e5d6769c4c32869ba696
jimmyjames88 <james.allen@fyidoctors.com>
2020-11-24 16:58:24 -0700

    global vs local vars

---
+++ js-variable-scope/js/app.js
@@ -0,0 +1,13 @@
+let message = 'Hello world!!!';
+let newMessages = 5;
+
+function checkMessages() {
+    alert(message);
+    newMessages = 0;
+}
+
+let checkMessagesButton = document.querySelector('.check-messages-button');
+checkMessagesButton.addEventListener('click', function() {
+    checkMessages();
+    console.log(newMessages);
+});
