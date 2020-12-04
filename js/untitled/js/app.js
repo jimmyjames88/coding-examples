41a2e7979ec7c865e621068dcc1854f5574c4f40
jimmyjames88 <james.allen@fyidoctors.com>
2020-12-04 12:44:09 -0700

    workshop - vars, arrays, loops

---
+++ js/untitled/js/app.js
@@ -0,0 +1,13 @@
+// variables (w9b) and arrays (w9e)
+let pageTitle = 'Random Dog Photos!!!';
+let dogPhotos = [
+    'https://random.dog/808396ad-2ba6-4b2a-8024-7b6cac8dad93.mp4',
+    'https://random.dog/afba068f-e94f-4cdf-b21e-659e5ff12b91.jpg',
+    'https://random.dog/3c4ae6f2-7f29-45e0-94f7-c561d3523f04.mp4',
+    'https://random.dog/3c8233e8-c1d0-4214-9848-7658e6d9807e.jpg' 
+];
+
+// loops (w9f)
+for(let i = 0; i < dogPhotos.length; i++) {
+    console.log(dogPhotoURLs[i]);
+} 
