16a4f4a5bbf633eed8eabe32bcc23e9db62eb537
jimmyjames88 <james.allen@fyidoctors.com>
2020-12-19 11:54:06 -0700

    kanye rest axios example

---
+++ js/vue-kanye-rest/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <kanye-rest></kanye-rest>
+    </div>
+</template>
+
+<script>
+import KanyeRest from './components/KanyeRest';
+
+export default {
+    name: "App",
+    components: {
+        KanyeRest
+    }
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ js/vue-kanye-rest/src/components/KanyeRest.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="card">
+        <div class="card-content">
+            <button class="button is-primary" @click="loadQuote">Load Quote</button>
+            <p v-if="errorMessage" class="notification is-danger">{{ errorMessage }}</p>
+            <p v-if="currentQuote" class="notification">{{ currentQuote }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'KanyeRest',
+    data() {
+        return {
+            currentQuote: '',
+            errorMessage: ''
+        }
+    },
+
+    methods: {
+        loadQuote() {
+            axios.request({
+                type: 'get',
+                url: 'https://api.kanye.rest',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                params: {
+                    format: 'text',
+                }
+            }).then((response) => { 
+                console.log(response);
+                this.currentQuote = response.data.quote;
+            }).catch((error) => {
+                this.errorMessage = error;
+                console.log(error);
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
